229c9841a48a0e2b227b3791fec4f6b6f0a873cf
Satish01-oss <rs1599696@gamil.com>
2026-06-04 23:56:42 +0530

    Configure frontend for production backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,13 +14,11 @@ const userRoutes = require('./routes/user.route');
 const fraud = require('./routes/fraud.route');
 const transactionRoutes = require('./routes/transaction.route');
 
+const app = express();
 app.use(cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+    origin: true,
+    credentials: true
 }));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
