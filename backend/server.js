ab20e83dfc3d92a4469191bb8271af951405d741
Satish01-oss <rs1599696@gamil.com>
2026-06-04 23:17:02 +0530

    Add fallback port

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const app =  require('./src/app')
 const connectDB = require('./src/db/db');
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, async () => {
     await connectDB();
     console.log(`Server running on port ${PORT}`);
